398438832af1b74398d02a0e36a6a04153b2b175
Alexander Farlinger <alexanderfarlinger@gmail.com>
2021-12-10 12:45:07 -0800

    added additional changes to the media screen stuff like changing section height and font size

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
+//variables
 const toggleButton = document.querySelector(".toggle");
 const body = document.querySelector(".light-theme");
 
 const submitButton = document.querySelector("#frm-btn");
 const email = document.querySelector("#email");
 const nameInput = document.querySelector("#name");
-const message = document.querySelector("#msg")
-let submissions = {};
+const message = document.querySelector("#msg");
+
+//event handlers
 toggleButton.addEventListener("click", function() {
     if (body.classList == "light-theme") {
         body.removeAttribute("light-theme")
